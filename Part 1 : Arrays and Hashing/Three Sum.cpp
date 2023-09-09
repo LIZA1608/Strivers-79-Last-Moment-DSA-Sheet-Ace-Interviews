a971079764d64405b6cd5c4d30604cc81cf462c5
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-09-09 14:50:35 +0530

    Update Three Sum.cpp

--- Part 1 : Arrays and Hashing/Three Sum.cpp
+++ Part 1 : Arrays and Hashing/Three Sum.cpp
@@ -1,4 +1,32 @@
-/*Two pointer approch */
+/*Two pointer approch T.C->O(nlongn)+O(n^2)*/
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    vector<vector<int>>ans;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n;i++){
+        if(i>0 && arr[i]==arr[i-1]) continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k){
+            int sum=arr[i]+arr[j]+arr[k];
+            if(sum<0){
+                j++;
+            }
+            else if(sum>0){
+                k--;
+            }
+            else{
+                vector<int>temp={arr[i],arr[j],arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while( j<k && arr[j]==arr[j-1]) j++;
+                while(j<k && arr[k]==arr[k+1]) k--;
+            }
+        }
+    }
+    return ans;
+} 
 /*hashing   T.c->O(N^2) ,SC->O(N)*/
 #include<bits/stdc++.h>
 vector<vector<int>> triplet(int n, vector<int> &arr)
